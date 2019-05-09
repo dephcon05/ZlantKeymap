4d76ea64da46e3a49c41488698dcdc77cb2dff8f
dephcon05 <42864181+dephcon05@users.noreply.github.com>
2019-05-09 00:32:20 -0700

    Add tap dance actions.

--- keymap.c
+++ keymap.c
@@ -19,13 +19,19 @@ SEL = SAFE_RANGE
 ,CTRL3
 };
 
+enum {
+  TD_CAPS = 0,
+};
+qk_tap_dance_action_t tap_dance_actions[] = {
+  [TD_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, KC_CAPS)
+};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [BASE] = LAYOUT(
     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
     KC_TAB, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-    KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,  \
+    TD(TD_CAPS),  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,  \
     KC_LCTL, KC_LGUI, KC_LALT, MO(SUPER), MO(LOWER), KC_SPC,  MO(UPPER),   KC_LEFT, KC_RGHT, KC_DOWN,   KC_UP  \
   ),
 
