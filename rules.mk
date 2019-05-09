9ce18e84c235a776d37586eba29570c4b46a1b4e
dephcon05 <42864181+dephcon05@users.noreply.github.com>
2019-05-09 00:34:22 -0700

    Add tap dance enable

--- rules.mk
+++ rules.mk
@@ -54,3 +54,4 @@ NKRO_ENABLE = yes		# USB Nkey Rollover - if this doesn't work, see here: https:/
 BACKLIGHT_ENABLE = no  # Enable keyboard backlight functionality
 AUDIO_ENABLE = no
 RGBLIGHT_ENABLE = yes
+TAP_DANCE_ENABLE = yes
